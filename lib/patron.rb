c7feae4180fbc8b6089ca03a17bf0158ddea5ac1
Carolyn Huynh & Diane Douglas <student@epicodus.com>
2014-08-12 14:42:41 -0700

    create save all and equal method for patron class

--- lib/patron.rb
+++ lib/patron.rb
@@ -6,4 +6,23 @@ class Patron
     @name = attributes['name']
     @id = attributes['id'].to_i
   end
+
+  def save
+    results = DB.exec("INSERT INTO patrons (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    patrons = []
+    results = DB.exec("SELECT * FROM patrons;")
+    results.each do |patron|
+      patrons << Patron.new(patron)
+    end
+    patrons
+  end
+
+  def ==(another_patron)
+    self.name == another_patron.name && self.id == another_patron.id
+  end
+
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -6,4 +6,10 @@ describe Patron do
     test_patron = Patron.new({'name' => 'Dustin'})
     expect(test_patron).to be_an_instance_of Patron
   end
+
+  it 'saves a Patron to the database' do
+    test_patron = Patron.new({'name' => 'Dustin'})
+    test_patron.save
+    expect(Patron.all).to eq [test_patron]
+  end
 end
