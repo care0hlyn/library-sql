9c12acc65dff40f0fa3679b03926a6e6a36b62b6
Carolyn Huynh & Diane Douglas <student@epicodus.com>
2014-08-12 10:33:14 -0700

    add save all and equals method for author class

---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Author
+
+  attr_reader :name, :id
+
+  def initialize(attributes)
+    @name = attributes['name']
+    @id = attributes['id'].to_i
+  end
+
+  def update_name(name)
+    DB.exec("UPDATE authors SET name = '#{@name}';")
+    @name = name
+  end
+
+  def save
+    results = DB.exec("INSERT INTO authors (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM authors;")
+    authors = []
+    results.each do |author_info|
+      authors << Author.new(author_info)
+    end
+    authors
+  end
+
+  def ==(another_author)
+    another_author.name == self.name && another_author.id == self.id
+  end
+end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,1 +1,23 @@
 require 'library_spec_helper'
+
+
+describe Author do
+
+  it 'should initialize the name of the author' do
+    test_author = Author.new({'name' => "JK Rowling"})
+    expect(test_author).to be_an_instance_of Author
+    expect(test_author.name).to eq "JK Rowling"
+  end
+
+  it 'should update the name of the author' do
+    test_author = Author.new({'name' => "JK Rowling"})
+    test_author.update_name('Isaac Asimov')
+    expect(test_author.name).to eq 'Isaac Asimov'
+  end
+
+  it 'should save the author to the database' do
+    test_author = Author.new({'name' => "JK Rowling"})
+    test_author.save
+    expect(Author.all).to eq [test_author]
+    end
+end
